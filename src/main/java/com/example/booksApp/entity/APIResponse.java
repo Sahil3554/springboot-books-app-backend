093d6c465509752a0a5e439e2dd66461c936ed1c
sahil3554 <sahiltagra3554@gmail.com>
2024-12-06 17:28:39 +0530

    Added Lombok in entities

--- src/main/java/com/example/booksApp/entity/APIResponse.java
+++ src/main/java/com/example/booksApp/entity/APIResponse.java
@@ -1,34 +1,13 @@
 package com.example.booksApp.entity;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
+@Data
 public class APIResponse<T> {
     private String message;
     private String error;
     private T data;
 
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    public String getError() {
-        return error;
-    }
-
-    public void setError(String error) {
-        this.error = error;
-    }
-
-    public T getData() {
-        return data;
-    }
-
-    public void setData(T data) {
-        this.data = data;
-    }
 }
--- src/main/java/com/example/booksApp/entity/Book.java
+++ src/main/java/com/example/booksApp/entity/Book.java
@@ -1,42 +1,25 @@
 package com.example.booksApp.entity;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.Getter;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "books")
+@Data
 public class Book {
     @Id
+    @Getter(AccessLevel.NONE)
     public ObjectId id;
     public String name;
     public Integer pagesCount;
-
-//    public ObjectId getId() {
-//        return id;
-//    }
 
     @JsonProperty("id")
     public String getId() {
         return id != null ? id.toHexString() : null; // Convert ObjectId to its string representation
     }
-    public void setId(ObjectId id) {
-        this.id = id;
-    }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Integer getPagesCount() {
-        return pagesCount;
-    }
-
-    public void setPagesCount(Integer pagesCount) {
-        this.pagesCount = pagesCount;
-    }
 }
